47647ff971c43987ef7f604bb598bad76448cf4b
Alexander <dr.ambercat@gmail.com>
2023-01-05 16:09:33 +0300

    redux saga implementation

--- saga-example/src/lib/store.ts
+++ saga-example/src/lib/store.ts
@@ -1,5 +1,50 @@
-import { createStore } from 'redux'
-import { Todo } from './api'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from '@redux-saga/core'
+import { Todo, getTodos, updateTodo, deleteTodo, createTodo } from './api'
+import { put, takeEvery } from 'redux-saga/effects'
+
+function* getTodosAction() {
+  const todos: Todo[] = yield getTodos()
+  yield put({ type: 'TODOS_FETCH_SUCCEEDED', payload: todos })
+}
+
+function* updateTodoAction({
+  payload,
+}: {
+  type: 'UPDATE_TODO_REQUESTED'
+  payload: Todo
+}) {
+  yield updateTodo(payload)
+  yield put({ type: 'TODOS_FETCH_REQUESTED' })
+}
+
+function* deleteTodoAction({
+  payload,
+}: {
+  type: 'DELETE_TODO_REQUESTED'
+  payload: Todo
+}) {
+  yield deleteTodo(payload)
+  yield put({ type: 'TODOS_FETCH_REQUESTED' })
+}
+
+function* createTodoAction({
+  payload,
+}: {
+  type: 'CREATE_TODO_REQUESTED'
+  payload: string
+}) {
+  yield createTodo(payload)
+  yield put({ type: 'TODOS_FETCH_REQUESTED' })
+}
+
+// rootSaga
+function* rootSaga() {
+  yield takeEvery('TODOS_FETCH_REQUESTED', getTodosAction)
+  yield takeEvery('UPDATE_TODO_REQUESTED', updateTodoAction)
+  yield takeEvery('DELETE_TODO_REQUESTED', deleteTodoAction)
+  yield takeEvery('CREATE_TODO_REQUESTED', createTodoAction)
+}
 
 const reducer = (
   state: Todo[] = [],
@@ -12,3 +57,31 @@ const reducer = (
       return state
   }
 }
+
+const sagaMiddleware = createSagaMiddleware()
+
+export const store = createStore(reducer, applyMiddleware(sagaMiddleware))
+
+sagaMiddleware.run(rootSaga)
+
+export const selectTodos = (state: Todo[]) => state
+
+export const fetchTodos = () => ({ type: 'TODOS_FETCH_REQUESTED' })
+
+export const toggleTodo = (todo: Todo) => ({
+  type: 'UPDATE_TODO_REQUESTED',
+  payload: {
+    ...todo,
+    done: !todo.done,
+  },
+})
+
+export const removeTodo = (todo: Todo) => ({
+  type: 'DELETE_TODO_REQUESTED',
+  payload: todo,
+})
+
+export const addTodo = (text: string) => ({
+  type: 'CREATE_TODO_REQUESTED',
+  payload: text,
+})
